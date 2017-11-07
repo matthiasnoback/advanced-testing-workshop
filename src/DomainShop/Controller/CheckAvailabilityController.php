b735241c1a60a150ee9364bd75a37d60a3290513
Matthias Noback <matthiasnoback@gmail.com>
2017-11-07 11:45:48 +0100

    Add real domain availability check

--- src/DomainShop/Controller/CheckAvailabilityController.php
+++ src/DomainShop/Controller/CheckAvailabilityController.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace DomainShop\Controller;
 
+use Novutec\WhoisParser\Parser;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Zend\Expressive\Template\TemplateRendererInterface;
@@ -25,8 +26,13 @@ final class CheckAvailabilityController implements MiddlewareInterface
         $formData = $request->getParsedBody();
         $domainName = $formData['domain_name'];
 
+        $parser = new Parser();
+        $result = $parser->lookup($domainName);
+
+        $isAvailable = !$result->registered;
+
         $response->getBody()->write($this->renderer->render('availability.html.twig', [
-            'isAvailable' => true,
+            'isAvailable' => $isAvailable,
             'domainName' => $domainName
         ]));
 
