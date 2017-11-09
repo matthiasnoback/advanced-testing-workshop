65854fd5739f62cc353022493efbdf6e6cbe78c3
Matthias Noback <matthiasnoback@gmail.com>
2017-11-09 10:19:23 +0100

    Improve test script
    Fixes #22 and #23

---
+++ test/Unit/DummyUnitTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyUnitTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_works()
+    {
+        self::assertTrue(true);
+    }
+}
