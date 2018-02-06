6a84f2bd224f7e78f37d888c54a6b05e1a12cc3d
Matthias Noback <matthiasnoback@gmail.com>
2018-02-06 11:54:24 +0100

    Generate code coverage for unit and integration tests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -4,8 +4,8 @@ set -eu
 
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml -f docker-compose.test.yml"
 ${DOCKER_COMPOSE} run --rm devtools /bin/bash -c \
-    "vendor/bin/phpunit --testsuite unit -v \
-     && vendor/bin/phpunit --testsuite integration -v \
+    "vendor/bin/phpunit --testsuite unit --coverage-php var/coverage/unit.cov -v \
+     && vendor/bin/phpunit --testsuite integration --coverage-php var/coverage/integration.cov -v \
      && vendor/bin/behat --suite acceptance -vvv"
 ${DOCKER_COMPOSE} up -d web
 ${DOCKER_COMPOSE} run --rm devtools /bin/bash -c "vendor/bin/behat --suite system -vvv"
