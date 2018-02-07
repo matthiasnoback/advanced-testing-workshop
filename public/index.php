9cbfdbdb5142f73a5421fade331c48ed2afa913d
Matthias Noback <matthiasnoback@gmail.com>
2018-02-07 21:17:36 +0100

    Move the Behat extensions to real packages

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 declare(strict_types=1);
 
 use Interop\Container\ContainerInterface;
-use LiveCodeCoverage\LiveCodeCoverage;
+use LiveCodeCoverage\RemoteCodeCoverage;
 use Zend\Expressive\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$shutDownCodeCoverage = LiveCodeCoverage::bootstrapRemoteCoverage(
+$shutDownCodeCoverage = RemoteCodeCoverage::bootstrap(
     (bool)getenv('CODE_COVERAGE_ENABLED'),
     sys_get_temp_dir(),
     __DIR__ . '/../phpunit.xml.dist'
