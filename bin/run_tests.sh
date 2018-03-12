85babb5ac83bc61063ae25a7253b52c47d2b5c5d
Matthias Noback <matthiasnoback@gmail.com>
2018-03-12 14:31:33 +0100

    Add unit and integration tests

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,6 +3,10 @@
 set -eu
 
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml -f docker-compose.test.yml"
+${DOCKER_COMPOSE} run --rm devtools /bin/bash -c \
+    "vendor/bin/phpunit --testsuite unit -v \
+     && vendor/bin/phpunit --testsuite integration -v \
+     && vendor/bin/behat --suite acceptance -vvv"
 ${DOCKER_COMPOSE} up -d web
-${DOCKER_COMPOSE} run --rm devtools /bin/bash -c "vendor/bin/phpunit && vendor/bin/behat -vvv"
+${DOCKER_COMPOSE} run --rm devtools /bin/bash -c "vendor/bin/behat --suite end_to_end -vvv"
 ${DOCKER_COMPOSE} stop web
---
+++ test/Integration/DummyIntegrationTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyIntegrationTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_works()
+    {
+        self::assertTrue(true);
+    }
+}
