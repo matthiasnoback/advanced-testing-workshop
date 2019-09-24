f3dcde52487af5221dfa3b81e37c5afb462f7ce0
Matthias Noback <matthiasnoback@gmail.com>
2019-09-24 10:47:39 +0200

    Fix test script for Windows using Git Bash

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -2,14 +2,16 @@
 
 set -eu
 
+# Note: we're using double slashes in front of /bin/bash to make this script work on Git Bash on Windows as well.
+
 DOCKER_COMPOSE="docker-compose -f docker-compose.yml -f docker-compose.test.yml"
-${DOCKER_COMPOSE} run --rm devtools /bin/bash -c \
+${DOCKER_COMPOSE} run --rm devtools //bin/bash -c \
     "vendor/bin/phpunit --testsuite unit --coverage-php var/coverage/unit.cov -v \
      && vendor/bin/phpunit --testsuite integration --coverage-php var/coverage/integration.cov -v \
      && vendor/bin/behat --suite acceptance -vvv"
 ${DOCKER_COMPOSE} up -d web
-${DOCKER_COMPOSE} run --rm devtools /bin/bash -c "vendor/bin/behat --suite system -vvv"
+${DOCKER_COMPOSE} run --rm devtools //bin/bash -c "vendor/bin/behat --suite system -vvv"
 ${DOCKER_COMPOSE} stop web
 
-docker-compose run --rm devtools /bin/bash -c "phpcov merge --html=var/coverage/html var/coverage"
+docker-compose run --rm devtools //bin/bash -c "phpcov merge --html=var/coverage/html var/coverage"
 echo "Coverage report generated in var/coverage/html"
